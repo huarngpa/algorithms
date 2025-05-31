9b7e189f14105cf23abad1c4aae835bc968d4d9d
Patrick Huarng <huarngpa@gmail.com>
2025-05-31 10:58:25 -0400

    Banking system

--- src/main/java/com/github/huarngpa/leetcode/coinbase/BankingSystem.java
+++ src/main/java/com/github/huarngpa/leetcode/coinbase/BankingSystem.java
@@ -1,6 +1,79 @@
 package com.github.huarngpa.leetcode.coinbase;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * You have been tasked with writing a program for a popular bank that will automate all its
+ * incoming transactions (transfer, deposit, and withdraw). The bank has n accounts numbered from 1
+ * to n. The initial balance of each account is stored in a 0-indexed integer array balance, with
+ * the (i + 1)th account having an initial balance of balance[i].
+ *
+ * <p>Execute all the valid transactions. A transaction is valid if:
+ *
+ * <p>The given account number(s) are between 1 and n, and The amount of money withdrawn or
+ * transferred from is less than or equal to the balance of the account. Implement the Bank class:
+ *
+ * <p>Bank(long[] balance) Initializes the object with the 0-indexed integer array balance. boolean
+ * transfer(int account1, int account2, long money) Transfers money dollars from the account
+ * numbered account1 to the account numbered account2. Return true if the transaction was
+ * successful, false otherwise. boolean deposit(int account, long money) Deposit money dollars into
+ * the account numbered account. Return true if the transaction was successful, false otherwise.
+ * boolean withdraw(int account, long money) Withdraw money dollars from the account numbered
+ * account. Return true if the transaction was successful, false otherwise.
+ *
+ * <p>Example 1:
+ *
+ * <p>Input ["Bank", "withdraw", "transfer", "deposit", "transfer", "withdraw"] [[[10, 100, 20, 50,
+ * 30]], [3, 10], [5, 1, 20], [5, 20], [3, 4, 15], [10, 50]] Output [null, true, true, true, false,
+ * false]
+ *
+ * <p>Explanation Bank bank = new Bank([10, 100, 20, 50, 30]); bank.withdraw(3, 10); // return true,
+ * account 3 has a balance of $20, so it is valid to withdraw $10. // Account 3 has $20 - $10 = $10.
+ * bank.transfer(5, 1, 20); // return true, account 5 has a balance of $30, so it is valid to
+ * transfer $20. // Account 5 has $30 - $20 = $10, and account 1 has $10 + $20 = $30.
+ * bank.deposit(5, 20); // return true, it is valid to deposit $20 to account 5. // Account 5 has
+ * $10 + $20 = $30. bank.transfer(3, 4, 15); // return false, the current balance of account 3 is
+ * $10, // so it is invalid to transfer $15 from it. bank.withdraw(10, 50); // return false, it is
+ * invalid because account 10 does not exist.
+ *
+ * <p>Constraints:
+ *
+ * <p>n == balance.length 1 <= n, account, account1, account2 <= 105 0 <= balance[i], money <= 1012
+ * At most 104 calls will be made to each function transfer, deposit, withdraw.
+ */
 public class BankingSystem {
 
-  /** */
+  Map<Integer, Long> balances = new HashMap<>();
+
+  public BankingSystem(long[] balance) {
+    for (int i = 1; i <= balance.length; i++) {
+      balances.put(i, balance[i - 1]);
+    }
+  }
+
+  public boolean transfer(int account1, int account2, long money) {
+    if (!balances.containsKey(account1) || !balances.containsKey(account2)) {
+      return false;
+    }
+    return withdraw(account1, money) && deposit(account2, money);
+  }
+
+  public boolean deposit(int account, long money) {
+    Long balance = balances.get(account);
+    if (balance == null) {
+      return false;
+    }
+    balances.put(account, balance + money);
+    return true;
+  }
+
+  public boolean withdraw(int account, long money) {
+    Long balance = balances.get(account);
+    if (balance == null || balance < money) {
+      return false;
+    }
+    balances.put(account, balance - money);
+    return true;
+  }
 }
