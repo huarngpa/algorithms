665aa1e37e0ad67dea62af3cdbcea4f09d5b4da4
Patrick Huarng <huarngpa@gmail.com>
2025-06-28 23:36:44 -0400

    Iterator with concurrency

--- src/main/java/com/github/huarngpa/leetcode/coinbase/NestedIterator.java
+++ src/main/java/com/github/huarngpa/leetcode/coinbase/NestedIterator.java
@@ -3,6 +3,8 @@ package com.github.huarngpa.leetcode.coinbase;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Stack;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 /**
  * You are given a nested list of integers nestedList. Each element is either an integer or a list
@@ -31,9 +33,12 @@ public class NestedIterator implements Iterator<Integer> {
 
   private Stack<NestedInteger> stack = new Stack<>();
 
-  // Extended, see update
+  // Extended, see `update` method
   private List<NestedInteger> nestedList; // Store original list
   private int processedIndex = 0;
+
+  // Extended, see `stream` method
+  private final Object lock = new Object();
 
   public NestedIterator(List<NestedInteger> nestedList) {
     // Push elements in reverse order into the stack
@@ -46,23 +51,27 @@ public class NestedIterator implements Iterator<Integer> {
 
   @Override
   public boolean hasNext() {
-    // Process stack until top is an integer or stack is empty
-    while (!stack.isEmpty() && !stack.peek().isInteger()) {
-      NestedInteger top = stack.pop();
-      // Push list elements in reverse order
-      for (int i = top.getList().size() - 1; i >= 0; i--) {
-        stack.push(top.getList().get(i));
+    synchronized (lock) {
+      // Process stack until top is an integer or stack is empty
+      while (!stack.isEmpty() && !stack.peek().isInteger()) {
+        NestedInteger top = stack.pop();
+        // Push list elements in reverse order
+        for (int i = top.getList().size() - 1; i >= 0; i--) {
+          stack.push(top.getList().get(i));
+        }
       }
+      return !stack.isEmpty();
     }
-    return !stack.isEmpty();
   }
 
   @Override
   public Integer next() {
-    // Extended
-    processedIndex++;
-    // Assumes hasNext() is `true`
-    return stack.pop().getInteger();
+    synchronized (lock) {
+      // Extended
+      processedIndex++;
+      // Assumes hasNext() is `true`
+      return stack.pop().getInteger();
+    }
   }
 
   /**
@@ -101,5 +110,39 @@ public class NestedIterator implements Iterator<Integer> {
         stack.push(nestedList.get(i));
       }
     }
+  }
+
+  /**
+   * Make the NestedIterator thread-safe for concurrent access by multiple threads calling hasNext
+   * and next. Add a stream() method that returns an Iterator<Integer> for asynchronous streaming,
+   * simulating a blockchain data stream. How would you ensure thread safety and handle large
+   * datasets efficiently?
+   */
+  public Iterator<Integer> stream() {
+    // This is a bit unnecessary, don't bother studying this part!
+    BlockingQueue<Integer> queue = new ArrayBlockingQueue<>(100);
+    new Thread(
+            () -> {
+              while (hasNext()) {
+                try {
+                  queue.put(next());
+                } catch (InterruptedException e) {
+                  Thread.currentThread().interrupt();
+                }
+              }
+            })
+        .start();
+    return new Iterator<Integer>() {
+
+      @Override
+      public boolean hasNext() {
+        return !queue.isEmpty();
+      }
+
+      @Override
+      public Integer next() {
+        return queue.poll();
+      }
+    };
   }
 }
